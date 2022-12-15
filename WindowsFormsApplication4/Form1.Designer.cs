b8d45a83c423ba30ab2e3b34cc26358fc3da7fd5
kzalles <104508652+kzalles@users.noreply.github.com>
2022-12-14 20:14:17 -0400

    Add files via upload

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Tomato;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(881, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.savebtn_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Blue;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(881, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 51);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Cargar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.loadbtn_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.BlueViolet;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(881, 86);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 51);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Convertir";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.converterbtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DarkGray;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(853, 531);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1049, 557);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap Image, Image2;
+        private BitmapData ImageData, ImageData2;
+        private byte[] buffer, buffer2;
+        private int b, g, r, r_x, g_x, b_x, r_y, g_y, b_y, grayscale, location, location2;
+        private sbyte weight_x, weight_y;
+        private sbyte[,] weights_x;
+        private sbyte[,] weights_y;
+        private IntPtr pointer, pointer2;
+        public Form1()
+        {
+            InitializeComponent();
+            weights_x = new sbyte[,] { { 1, 0, -1 }, { 2, 0, -2 }, { 1, 0, -1 } };
+            weights_y = new sbyte[,] { { 1, 2, 1 }, { 0, 0, 0 }, { -1, -2, -1 } };
+        }
+
+        private void savebtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(sfd.FileName, ImageFormat.Bmp);
+            }
+        }
+
+        private void loadbtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Image = new Bitmap(ofd.FileName);
+                Image2 = new Bitmap(Image.Width, Image.Height);
+            }
+            pictureBox1.Image = Image;
+        }
+
+        private void converterbtn_Click(object sender, EventArgs e)
+        {
+            ImageData = Image.LockBits(new Rectangle(0, 0, Image.Width, Image.Height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            ImageData2 = Image2.LockBits(new Rectangle(0, 0, Image.Width, Image.Height), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+            buffer = new byte[ImageData.Stride * Image.Height];
+            buffer2 = new byte[ImageData.Stride * Image.Height];
+            pointer = ImageData.Scan0;
+            pointer2 = ImageData2.Scan0;
+            Marshal.Copy(pointer, buffer, 0, buffer.Length);
+            for (int y = 0; y < Image.Height; y++)
+            {
+                for (int x = 0; x < Image.Width * 3; x += 3)
+                {
+                    r_x = g_x = b_x = 0; //reset the gradients in x-direcion values
+                    r_y = g_y = b_y = 0; //reset the gradients in y-direction values
+                    location = x + y * ImageData.Stride; //to get the location of any pixel >> location = x + y * Stride
+                    for (int yy = -(int)Math.Floor(weights_y.GetLength(0) / 2.0d), yyy = 0; yy <= (int)Math.Floor(weights_y.GetLength(0) / 2.0d); yy++, yyy++)
+                    {
+                        if (y + yy >= 0 && y + yy < Image.Height) //to prevent crossing the bounds of the array
+                        {
+                            for (int xx = -(int)Math.Floor(weights_x.GetLength(1) / 2.0d) * 3, xxx = 0; xx <= (int)Math.Floor(weights_x.GetLength(1) / 2.0d) * 3; xx += 3, xxx++)
+                            {
+                                if (x + xx >= 0 && x + xx <= Image.Width * 3 - 3) //to prevent crossing the bounds of the array
+                                {
+                                    location2 = x + xx + (yy + y) * ImageData.Stride; //to get the location of any pixel >> location = x + y * Stride
+                                    weight_x = weights_x[yyy, xxx];
+                                    weight_y = weights_y[yyy, xxx];
+                                    //applying the same weight to all channels
+                                    b_x += buffer[location2] * weight_x;
+                                    g_x += buffer[location2 + 1] * weight_x; //G_X
+                                    r_x += buffer[location2 + 2] * weight_x;
+                                    b_y += buffer[location2] * weight_y;
+                                    g_y += buffer[location2 + 1] * weight_y;//G_Y
+                                    r_y += buffer[location2 + 2] * weight_y;
+                                }
+                            }
+                        }
+                    }
+                    //getting the magnitude for each channel
+                    b = (int)Math.Sqrt(Math.Pow(b_x, 2) + Math.Pow(b_y, 2));
+                    g = (int)Math.Sqrt(Math.Pow(g_x, 2) + Math.Pow(g_y, 2));//G
+                    r = (int)Math.Sqrt(Math.Pow(r_x, 2) + Math.Pow(r_y, 2));
+
+                    if (b > 255) b = 255;
+                    if (g > 255) g = 255;
+                    if (r > 255) r = 255;
+
+                    //getting grayscale value
+                    grayscale = (b + g + r) / 3;
+
+                    //thresholding to clean up the background
+                    //if (grayscale < 80) grayscale = 0;
+                    buffer2[location] = (byte)grayscale;
+                    buffer2[location + 1] = (byte)grayscale;
+                    buffer2[location + 2] = (byte)grayscale;
+                    //thresholding to clean up the background
+                    //if (b < 100) b = 0;
+                    //if (g < 100) g = 0;
+                    //if (r < 100) r = 0;
+
+                    //buffer2[location] = (byte)b;
+                    //buffer2[location + 1] = (byte)g;
+                    //buffer2[location + 2] = (byte)r;
+                }
+            }
+            Marshal.Copy(buffer2, 0, pointer2, buffer.Length);
+            Image.UnlockBits(ImageData);
+            Image2.UnlockBits(ImageData2);
+            pictureBox1.Image = Image2;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        
+    }
+}
